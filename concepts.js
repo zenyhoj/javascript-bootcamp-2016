ba6c6d3be2d0a22e7acabfb3d65b7d302c39bf85
zenyhoj <joe.balingit@gmail.com>
2018-01-05 14:17:53 +0800

    scopes and closures

--- concepts.js
+++ concepts.js
@@ -1,26 +1,20 @@
-//Scope Chain
+/*//Scope Chain
 //concept: the function looks for the outer environment(global variable declarations) for variable values if it did not find any inside the function()
 //in this example, function b() will look for myVar values in the global environment so it will output as 1
 
 function b(){
    console.log(myVar);
 }
-
 function a(){
    console.log(myVar);
    b();
 }
-
-
-
-
 var myVar = 2;
 console.log(myVar);
 var myVar = 3;//this will be ignored
 var myVar = 1;
 
-a();
-
+a();/*
 //Outputs:
 //2
 //1
@@ -28,7 +22,7 @@ a();
 //note: 2 is not get because it is being used in the console.log(myVar) global scope.
 //it will look for the inner variable value and not the top 1, which in this case is 1;
 
-/*//Asynchronous callbacks//
+/* Asynchronous callbacks
 //concept: even if the function loaded long, browser event (click event) that is triggered is still has to wait for the whole execute stack to finished
 //3 seconds delay function
 function waitThreeSeconds(){
@@ -36,6 +30,7 @@ function waitThreeSeconds(){
     while(new Date() < ms){}
     console.log('finished execution');
 }
+
 
 function clickHandler(){
     console.log('click event triggered');
@@ -45,4 +40,13 @@ function clickHandler(){
 document.addEventListener('click', clickHandler ); //event queu/loop; to be processed after all the execution stack has been processed
 
 waitThreeSeconds();
-console.log('finished execution');*/
+console.log('this is global execution');*/
+
+
+function greetName(){
+  return function name(){
+    console.log('hey' + name);
+  }
+  name();
+}
+greetName();
